2f5ddc5c37f96a4f034e2a5087043d958c434654
bondnoaa <1501630264@mil>
2019-02-26 10:43:17 -0800

    Addedd code for model averaging (Robinson Reach). and commented out pooled data code since ranking by site seams more appropriate.

--- Redd_logisticregression.R
+++ Redd_logisticregression.R
@@ -10,34 +10,35 @@ rr <- hab %>% filter(site == "rr")
 mrr <- hab %>% filter(site == "mrr")
 
 ####Logistic Regression Model Setup####
+#UPDATE: Keeping the two reaches separate makes more sence than pooling all the data together.
 #Proposing some basic models to make sure everything is functioning.
 # All data pooled together by site
-null <- glm(redd ~ 1,data = hab, family = binomial(link = "logit"))
-s <- glm(redd ~ site, data = hab, family = binomial(link = "logit")) #Placeholder if we want to include site as a parameter (typically in combination with other variables)
-dep <- glm(redd ~ dep, data = hab, family = binomial(link = "logit"))
-v <- glm(redd ~ vel, data = hab, family = binomial(link = "logit"))
-c <- glm(redd ~ csi, data = hab, family = binomial(link = "logit"))
-dar <- glm(redd ~ dar, data = hab, family = binomial(link = "logit"))
-w <- glm(redd ~ wsg, data = hab, family = binomial(link = "logit"))
-dar.c <- glm(redd ~ dar + csi, data = hab, family = binomial(link = "logit"))
-dar.w <- glm(redd ~ dar + wsg, data = hab, family = binomial(link = "logit"))
-c.w.inter <- glm(redd ~ csi + wsg + csi*wsg, data = hab, family = binomial(link = "logit")) #interaction
-
-#ADDITIONAL MODELS WITH SITE
-depS <- glm(redd ~ dep + site, data = hab, family = binomial(link = "logit"))
-vS <- glm(redd ~ vel + site, data = hab, family = binomial(link = "logit"))
-cS <- glm(redd ~ csi + site, data = hab, family = binomial(link = "logit"))
-darS <- glm(redd ~ dar + site, data = hab, family = binomial(link = "logit"))
-wS <- glm(redd ~ wsg + site, data = hab, family = binomial(link = "logit"))
-dar.cS <- glm(redd ~ dar + csi + site, data = hab, family = binomial(link = "logit"))
-dar.wS <- glm(redd ~ dar + wsg + site, data = hab, family = binomial(link = "logit"))
-c.w.interS <- glm(redd ~ csi + wsg + csi*wsg + site, data = hab, family = binomial(link = "logit")) #interaction
-
-raw1 <- AIC(dep, v, c, dar, w, dar.c, dar.w, c.w.inter, null)
-hab.output <- aictable(raw1,252)
-
-raw1b <- AIC(s, depS, vS, cS, darS, wS, dar.cS, dar.wS,c.w.interS) #WITH ADDITIONAL SITES
-hab.output2 <- aictable(raw1b,252)
+# null <- glm(redd ~ 1,data = hab, family = binomial(link = "logit"))
+# s <- glm(redd ~ site, data = hab, family = binomial(link = "logit")) #Placeholder if we want to include site as a parameter (typically in combination with other variables)
+# dep <- glm(redd ~ dep, data = hab, family = binomial(link = "logit"))
+# v <- glm(redd ~ vel, data = hab, family = binomial(link = "logit"))
+# c <- glm(redd ~ csi, data = hab, family = binomial(link = "logit"))
+# dar <- glm(redd ~ dar, data = hab, family = binomial(link = "logit"))
+# w <- glm(redd ~ wsg, data = hab, family = binomial(link = "logit"))
+# dar.c <- glm(redd ~ dar + csi, data = hab, family = binomial(link = "logit"))
+# dar.w <- glm(redd ~ dar + wsg, data = hab, family = binomial(link = "logit"))
+# c.w.inter <- glm(redd ~ csi + wsg + csi*wsg, data = hab, family = binomial(link = "logit")) #interaction
+# 
+# #ADDITIONAL MODELS WITH SITE
+# depS <- glm(redd ~ dep + site, data = hab, family = binomial(link = "logit"))
+# vS <- glm(redd ~ vel + site, data = hab, family = binomial(link = "logit"))
+# cS <- glm(redd ~ csi + site, data = hab, family = binomial(link = "logit"))
+# darS <- glm(redd ~ dar + site, data = hab, family = binomial(link = "logit"))
+# wS <- glm(redd ~ wsg + site, data = hab, family = binomial(link = "logit"))
+# dar.cS <- glm(redd ~ dar + csi + site, data = hab, family = binomial(link = "logit"))
+# dar.wS <- glm(redd ~ dar + wsg + site, data = hab, family = binomial(link = "logit"))
+# c.w.interS <- glm(redd ~ csi + wsg + csi*wsg + site, data = hab, family = binomial(link = "logit")) #interaction
+# 
+# raw1 <- AIC(dep, v, c, dar, w, dar.c, dar.w, c.w.inter, null)
+# hab.output <- aictable(raw1,252)
+# 
+# raw1b <- AIC(s, depS, vS, cS, darS, wS, dar.cS, dar.wS,c.w.interS) #WITH ADDITIONAL SITES
+# hab.output2 <- aictable(raw1b,252)
 
 #Best model of pooled data
 #summary(dep)
@@ -59,11 +60,17 @@ c.w.inter <- glm(redd ~ csi + wsg + csi*wsg, data = rr, family = binomial(link =
 raw2 <- AIC(dep, v, c, dar, w, dar.c, dar.w, c.w.inter, null)
 rr.output <- aictable(raw2,142)
 
-#Best model of Robinson data
-summary(dar.c)
-par(mfrow = c(2,2)) 
-plot(dar.c)
 write.csv(rr.output, file = "Data/LROutput_RR.csv", na = "")
+
+#Best model of Robinson data is both c.w.inter & dar.c
+#Model averaging using MuMIn library
+library(MuMIn) #install.packages(MuMIn)
+options(na.action = na.fail)
+mod.avg <- model.avg(c.w.inter,dar.c)
+summary(mod.avg)
+
+#write.csv(rr.param.output, file = "Data/LROutput_RR_modelaverageparam.csv", na = "")
+
 
 # MRR
 null <- glm(redd ~ 1,data = mrr, family = binomial(link = "logit"))
@@ -84,16 +91,10 @@ par(mfrow = c(2,2))
 plot(c.w.inter)
 write.csv(mrr.output, file = "Data/LROutput_MRR.csv", na = "")
 
+#Extra Code
 #Model generating using MuMIn library
 #  library(MuMIn) #install.packages(MuMIn)
 #  options(na.action = na.fail)
 #  
 #  all <- glm(redd ~ site + dar + vel + csi + wsg + dep, data = hab, family = binomial(link = "logit"))
 # allcomb <- dredge(all)
-
-#If model averaging is needed:
-#mod <- get.models(allcomb, subset = delta < 2 )# seq(nrow(allcomb)) 
-#mod.avg <- model.avg(mod)
-
-#'Best'model
-#summary(get.models(allcomb, 1)[[1]])
